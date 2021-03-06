65ee968de63887bb58a6b174c258d2d2907d1ed4
rdorff <beccadorff@gmail.com>
2021-03-06 11:23:16 -0700

    modified files for compatibility, moved resnet folder into simclr so we don't have to add it to the path

--- SimCLR/Models.py
+++ SimCLR/Models.py
@@ -1,11 +1,15 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
 
 from resnet.Models import ResNetSimCLR
-from SimCLR.Loss import NTCrossEntropyLoss
+from Loss import NTCrossEntropyLoss
 
 from tqdm import tqdm
+
+
 
 
 class SimCLR:
@@ -15,39 +19,52 @@ class SimCLR:
   does training using contrastive loss and returns a trained resnet
   '''
 
-  def __init__(self, in_channels=3, d_rep=1024, n_classes=10, *args, **kwargs):
+  def __init__(self, in_channels=3, d_rep=1024, n_classes=10, batch_size=128,
+               *args, **kwargs):
 
     # define the model
     self.model = ResNetSimCLR(in_channels, d_rep, n_classes, *args, **kwargs)
     self.model = self.model.to(self.device)
+    
+    self.batch_size = batch_size
   
   @property
   def device(self):
-    return torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+   
+      #return torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    return 'cpu'
 
-  def train(self, dataset, batch_size, temperature,
-            n_epochs=90, log_steps=100, ave_size=2000):
+  def load_data(self, dataset, s, input_shape):
+    # create data loader
+    data_loader = DataLoader(dataset, batch_size=self.batch_size, 
+                    drop_last=True, shuffle=True, num_workers=2)
     
-    # create train loader
-    train_loader = torch.utils.data.DataLoader(
-      dataset, batch_size=batch_size, shuffle=True, num_workers=2
-    )
+    return data_loader
+      
+
+  def train(self, dataloader, temperature, n_epochs=90, log_steps=100, 
+            ave_size=2000,save_size=10):
+    
+
 
     # trainers
-    criterion = NTCrossEntropyLoss(
-      temperature, batch_size, self.device
-    ).to(self.device)
+    criterion = NTCrossEntropyLoss(temperature, self.batch_size, 
+                                   self.device).to(self.device)
+    
     optimizer = torch.optim.Adam(self.model.parameters())
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-      optimizer, T_max=len(train_loader), eta_min=0, last_epoch=-1
+      optimizer, T_max=len(dataloader), eta_min=0, last_epoch=-1
     )
 
     losses = []
 
     for epoch in tqdm(range(n_epochs)):
       running_loss = 0
-      for i, (xis, xjs) in enumerate(train_loader):
-        self.optimizer.zero_grad()
+      i = 0
+      for (xis, xjs), _ in dataloader:
+        print("i",i)
+        i += 1
+        optimizer.zero_grad()
         
         xis = xis.to(self.device)
         xjs = xjs.to(self.device)
@@ -64,15 +81,15 @@ class SimCLR:
         
         # optimize
         loss.backward()
-        self.optimizer.step()
+        optimizer.step()
 
         # record loss
-        if i%ave_size == ave_size-1:
-          losses.append(running_loss / ave_size)
+        if i%save_size == 0:
+          losses.append(running_loss / save_size)
           running_loss = 0
 
       if epoch >= 10:
-        self.scheduler.step()
+        scheduler.step()
 
     return self.get_model(), losses
 

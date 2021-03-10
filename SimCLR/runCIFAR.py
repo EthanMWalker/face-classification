4358568255a5b1e3a10518aac7f65e13b93cd4f8
rdorff <beccadorff@gmail.com>
2021-03-10 10:01:51 -0700

    added cifar validation code

--- SimCLR/runCIFAR.py
+++ SimCLR/runCIFAR.py
@@ -9,29 +9,47 @@ Created on Fri Mar  5 19:16:11 2021
 
 import torchvision as tv
 from Augment import DataAugmentation, SimCLRDataTransform
-from Models import SimCLR
-
-s = 1
-input_shape = (96,96,3)
-batch_size = 1
-n_classes= 10
-in_channels = 3
-d_rep = 128
-temperature = .5
-
-# get transform pipeline
-transform = DataAugmentation(s, input_shape)
-augment = transform.augment()
-
-# load data
-trainset = tv.datasets.CIFAR10(root='TestData',
-                               train=True,download=True,transform=SimCLRDataTransform(augment))
+from Models import SimCLR, Validate
 
 
+def trainCIFAR():
+    s = 1
+    input_shape = (96,96,3)
+    temperature = .5
+    
+    # get transform pipeline
+    transform = DataAugmentation(s, input_shape)
+    augment = transform.augment()
+    
+    # load data
+    trainset = tv.datasets.CIFAR10(root='TrainData',
+                                   train=True,download=False,transform=SimCLRDataTransform(augment))
+    
+    
+    
+    simclr = SimCLR()
+    data = simclr.load_data(trainset,s,input_shape)
+    model,losses = simclr.train(data, temperature,n_epochs=1,ckpt_path='CIFAR10.tar')
+    return model, losses
 
-simclr = SimCLR()
-data = simclr.load_data(trainset,s,input_shape)
-model,losses = simclr.train(data, temperature,n_epochs=3,ckpt_path='CIFAR10.tar')
+def validateCIFAR(modelpath):
+    s = 1
+    input_shape = (96,96,3)  
+    
+    # get transform pipeline
+    transform = DataAugmentation(s, input_shape)
+    augment = transform.augment()
+    
+    # load data
+    testdata = tv.datasets.CIFAR10(root='TestData',
+                                   train=False,download=False,transform=SimCLRDataTransform(augment))
+    
+    simclr = Validate()
+    testloader = simclr.load_data(testdata,s,input_shape)
+    i_accuracy, j_accuracy =  simclr.validate_labels(modelpath,testloader)
+    return i_accuracy, j_accuracy
 
 
-
+if __name__ == "__main__":
+#    model, losses = trainCIFAR()
+    i_accuracy, j_accuracy = validateCIFAR('CIFAR10.tar81')

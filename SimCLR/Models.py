0695c577632c210261cfc7620be7c615c78b62ad
Ethan Supercomputer <ethan.walker830@gmail.com>
2021-03-16 18:57:58 -0600

    finally finished, awaiting final test of end to end training

--- SimCLR/Models.py
+++ SimCLR/Models.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, dataloader
+
+from torchlars import LARS
 
 from resnet.Models import ResNetSimCLR
 from Loss import NTCrossEntropyLoss
@@ -9,11 +11,11 @@ from Loss import NTCrossEntropyLoss
 from tqdm import tqdm
 
 class BaseModel:
-  def __init__(self, model=None, in_channels=3, d_rep=1024, n_classes=10,
+  def __init__(self, model=None, in_channels=3, n_classes=10,
               batch_size=128, *args, **kwargs):
     if model is None:
       # define the model
-      self.model = ResNetSimCLR(in_channels, d_rep, n_classes, *args, **kwargs)
+      self.model = ResNetSimCLR(in_channels, n_classes, *args, **kwargs)
     else:
       # use a pretrained model
       self.model = model
@@ -29,32 +31,31 @@ class BaseModel:
   def load_model(self, path):
     self.model.load_state_dict(torch.load(path)['model_state_dict'])
 
-  def load_data(self, dataset, s, input_shape):
+  def load_data(self, dataset):
     # create data loader
     data_loader = DataLoader(dataset, batch_size=self.batch_size, 
                     drop_last=True, shuffle=True, num_workers=2)
     return data_loader
 
 class Train(BaseModel):
-  def __init__(self, model=None, in_channels=3, d_rep=1024, n_classes=10,
-              batch_size=128):
+
+  def __init__(self, model=None, in_channels=3, n_classes=10,
+              batch_size=128, *args, **kwargs):
+
     super().__init__(
-      model, in_channels, d_rep, n_classes, batch_size, *args, **kwargs
+      model, in_channels, n_classes, batch_size, *args, **kwargs
     )
     
   def return_model(self):
-    return model.resnet, model.projection_head.layers[0]
+    return self.model.resnet, self.model.projection_head.layers[0]
   
   def train(self, dataloader, temperature, ckpt_path, n_epochs=90, 
-            log_steps=100, ave_size=2000,save_size=10,):
+          save_size=10):
     
     # trainers
     criterion = NTCrossEntropyLoss(temperature, self.batch_size, 
                                    self.device).to(self.device)
-    optimizer = torch.optim.Adam(self.model.parameters())
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-      optimizer, T_max=len(dataloader), eta_min=0, last_epoch=-1
-    )
+    optimizer = LARS(torch.optim.SGD(self.model.parameters(), lr=.1))
 
     losses = []
 
@@ -85,16 +86,13 @@ class Train(BaseModel):
           optimizer.step()
         
           # update tqdm
-          progress.set_description('loss:{:.4f}'.format(loss.item()))
+          progress.set_description('train loss:{:.4f}'.format(loss.item()))
           progress.update()
         
           # record loss
-          if i%save_size == 0:
+          if i%save_size == (save_size-1):
             losses.append(running_loss / save_size)
             running_loss = 0
-
-        if epoch >= 10:
-          scheduler.step()
         
         # save model
         if epoch%10 == 0:
@@ -104,14 +102,14 @@ class Train(BaseModel):
             'optimizer_state_dict': optimizer.state_dict(),
             'loss': loss,},ckpt_path)
     
- 
+
     return self.return_model(), losses
 
-class Fine_Tune(BaseModel):
-  def __init__(self, model=None, in_channels=3, d_rep=1024, n_classes=10,
-              batch_size=128):
+class FineTune(BaseModel):
+  def __init__(self, model=None, in_channels=3, n_classes=10,
+              batch_size=128, *args, **kwargs):
     super().__init__(
-      model, in_channels, d_rep, n_classes, batch_size, *args, **kwargs
+      model, in_channels, n_classes, batch_size, *args, **kwargs
     )
   
   def fine_tune(self, dataloader, ckpt_path, n_epochs=90, save_size=10):
@@ -124,7 +122,7 @@ class Fine_Tune(BaseModel):
     
     # only optimize the end of the projection head
     optimizer = torch.optim.Adam(
-      self.model.projection_head.layers[1:].parameters()
+      self.model.parameters()
     )
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
       optimizer, T_max=len(dataloader), eta_min=0, last_epoch=-1
@@ -157,7 +155,7 @@ class Fine_Tune(BaseModel):
           optimizer.step()
         
           # update tqdm
-          progress.set_description('loss:{:.4f}'.format(loss.item()))
+          progress.set_description('tune loss:{:.4f}'.format(loss.item()))
           progress.update()
         
           # record loss
@@ -185,50 +183,134 @@ class Validate(BaseModel):
   Takes a trained ResNetSimCLR model and computes accuracy 
   '''
   
-  def __init__(self, model=None, in_channels=3, d_rep=1024, n_classes=10,
-              batch_size=128):
+  def __init__(self, model=None, in_channels=3, n_classes=10,
+              batch_size=128, *args, **kwargs):
     super().__init__(
-      model, in_channels, d_rep, n_classes, batch_size, *args, **kwargs
+      model, in_channels, n_classes, batch_size, *args, **kwargs
     )
 
-  def validate(self, modelpath, dataloader):
+  def validate(self, dataloader):
     # validate test/validation set on trained model
     
     # load trained model
-    criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(self.model.parameters())
-    
-    checkpoint = torch.load(modelpath,map_location=torch.device(self.device))
-    self.model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    epoch = checkpoint['epoch']
-    loss = checkpoint['loss']
     
     self.model.eval()
     
-    losses = 0
-    i_accuracy = 0
-    j_accuracy = 0
+    total, correct = 0, 0
+    actual = []
+    predicted = []
 
     # validate 
-    for (xis, xjs), y in dataloader:
+    with tqdm(total=len(dataloader)) as progress:
+      for x, y in dataloader:
 
-      optimizer.zero_grad()
-    
-      xis = xis.to(self.device)
-      xjs = xjs.to(self.device)
-    
-      # Get representations and projections
-      his, zis = self.model(xis)
-      hjs, zjs= self.model(xjs)
-    
-      # normalize
-      # zis = F.normalize(zis)
-      # zjs = F.normalize(zjs)
+        optimizer.zero_grad()
+      
+        x = x.to(self.device)
+        y = y.to(self.device)
+      
+        # Get representations and projections
+        h, z = self.model(x)
 
-      if zis.argmax() == y:
-          i_accuracy += 1
-      if zjs.argmax() == y:
-          j_accuracy += 1
+        _, predict = torch.max(z.data, 1)
+      
+        total += y.shape[0]
+        correct += (predict == y).sum().item()
+
+        actual.extend(y.cpu())
+        predicted.extend(predict.cpu())
+
+        # update tqdm
+        progress.set_description('validating')
+        progress.update()
           
-    return i_accuracy, j_accuracy
+    return correct/total, actual, predicted
+
+
+class SimCLR:
+  def __init__(self, model=None, in_channels=3, n_classes=10, 
+              train_batch_size=1024, tune_batch_size=20, train_temp=.5):
+
+    if model is None:
+      self.trainer = Train(
+        None, in_channels, n_classes, train_batch_size
+      )
+    else:
+      self.trainer = Train(model, batch_size=train_batch_size)
+    
+    self.tune_batch_size = tune_batch_size
+    self.train_batch_size = train_batch_size
+    self.train_temp = train_temp
+  
+  def make_tuner(self, model):
+    '''
+    Parameters:
+      model (tuple(ResNet, nn.Linear)): the resnet and first layer of 
+        the projection head of for a simclr model
+    '''
+    resnet = model[0]
+    head = model[1]
+    in_channels = self.trainer.model.in_channels
+    n_classes = self.trainer.model.n_classes
+
+    model = ResNetSimCLR(
+      in_channels, n_classes, mlp_layers=3
+    )
+
+    model.resnet.load_state_dict(resnet.state_dict())
+    model.projection_head.layers[0].load_state_dict(head.state_dict())
+
+    self.tuner = FineTune(model, batch_size=self.tune_batch_size)
+  
+  def make_validator(self, model):
+    '''
+    Parameters:
+      model (ResNetSimCLR): a fill resnet with projection head
+    '''
+    self.validator = Validate(model, batch_size=self.tune_batch_size)
+  
+  def train(self, data, epochs, path):
+    dataloader = self.trainer.load_data(data)
+    model, losses = self.trainer.train(
+      dataloader, self.train_temp, path, epochs
+    )
+    return model, losses
+  
+  def tune(self, data, epochs, path):
+    dataloader = self.tuner.load_data(data)
+    model, losses = self.tuner.fine_tune(
+      dataloader, path, epochs
+    )
+    return model, losses
+  
+  def validate(self, data):
+    dataloader = self.validator.load_data(data)
+    acc, actual, predicted = self.validator.validate(dataloader)
+    return acc, actual, predicted
+
+  def full_model_maker(self, train_data, tune_data, val_data, n_cycles=10, 
+                      train_epochs=10, tune_epochs=50, 
+                      train_path='training.tar', tune_path='tuning.tar'):
+    
+    train_loss = []
+    tune_loss = []
+    accuracy = []
+
+    for i in range(n_cycles):
+      model, losses = self.train(train_data,train_epochs, train_path)
+      train_loss.extend(losses)
+
+      self.make_tuner(model)
+      model, losses = self.tune(tune_data, tune_epochs, tune_path)
+      tune_loss.extend(losses)
+
+      self.make_validator(self.tuner.model)
+      acc, actual, predicted = self.validate(val_data)
+      accuracy.append(acc)
+    
+    results = (
+      self.tuner.model, train_loss, tune_loss,
+      accuracy, actual, predicted
+    )
+    return results
--- SimCLR/runCIFAR.py
+++ SimCLR/runCIFAR.py
@@ -9,72 +9,151 @@ Created on Fri Mar  5 19:16:11 2021
 import torch
 import torchvision as tv
 from torchvision.transforms import transforms
-from Augment import DataAugmentation, SimCLRDataTransform
-from Models import SimCLR, Validate
+
+from Augment import TrainDataAugmentation, TuneDataAugmentation,\
+  SimCLRDataTransform, TuneDataTransform
+from Models import Train, FineTune, Validate, SimCLR
+from resnet.Models import ResNetSimCLR
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
 def finetuneCIFAR():
-    # fine tuning transform
-    transform = transforms.Compose(
-      [transforms.ToTensor(), transforms.Normalize((.5,.5,.5),(.5,.5,.5))]
-    )
-    # dataset for finetuning
-    tuneset = tv.datasets.CIFAR10(
-      root='TestData', train=True, download=True,
-      transform=transform
-    )
+  # fine tuning transform
+  transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((.5,.5,.5),(.5,.5,.5))]
+  )
+  # dataset for finetuning
+  tuneset = tv.datasets.CIFAR10(
+    root='TestData', train=True, download=True,
+    transform=transform
+  )
 
 
-    # make simclr with the pretrained model
-    simclr = SimCLR()
-    simclr.load_model('CIFAR10.tar')
+  # make simclr with the pretrained model
+  simclr = SimCLR()
+  simclr.load_model('CIFAR10.tar')
 
-    # do the fine tuning
-    data = simclr.load_data(tuneset, s, input_shape)
-    model, losses = simclr.fine_tune(data, 'CIFAR10-tune.tar', n_epochs=10)
+  # do the fine tuning
+  data = simclr.load_data(tuneset, s, input_shape)
+  model, losses = simclr.fine_tune(data, 'CIFAR10-tune.tar', n_epochs=10)
 
-    return model, losses
+  return model, losses
 
 
 
 def trainCIFAR():
-    s = 1
-    input_shape = (96,96,3)
-    temperature = .5
-    
-    # get transform pipeline
-    transform = DataAugmentation(s, input_shape)
-    augment = transform.augment()
-    
-    # load data
-    trainset = tv.datasets.CIFAR10(root='TrainData',
-                                   train=True,download=False,transform=SimCLRDataTransform(augment))
-    
-    
-    
-    simclr = SimCLR()
-    data = simclr.load_data(trainset,s,input_shape)
-    model,losses = simclr.train(data, temperature,n_epochs=1,ckpt_path='CIFAR10.tar')
-    return model, losses
+  s = 1
+  input_shape = (96,96,3)
+  temperature = .5
+  
+  # get transform pipeline
+  transform = TrainDataAugmentation(s, input_shape)
+  augment = transform.augment()
+  
+  # load data
+  trainset = tv.datasets.CIFAR10(
+    root='TrainData', train=True, download=False, 
+    transform=SimCLRDataTransform(augment)
+  )
+  
+  simclr = Train()
+  data = simclr.load_data(trainset)
+  model,losses = simclr.train(data, temperature,n_epochs=1,ckpt_path='CIFAR10.tar')
+  return model, losses
 
 def validateCIFAR(modelpath):
-    s = 1
-    input_shape = (96,96,3)  
-    
-    # get transform pipeline
-    transform = DataAugmentation(s, input_shape)
-    augment = transform.augment()
-    
-    # load data
-    testdata = tv.datasets.CIFAR10(root='TestData',
-                                   train=False,download=False,transform=SimCLRDataTransform(augment))
-    
-    simclr = Validate()
-    testloader = simclr.load_data(testdata,s,input_shape)
-    i_accuracy, j_accuracy =  simclr.validate_labels(modelpath,testloader)
-    return i_accuracy, j_accuracy
+  s = 1
+  input_shape = (96,96,3)  
+  
+  # get transform pipeline
+  transform = TuneDataAugmentation(s, input_shape)
+  augment = transform.augment()
+  
+  # load data
+  testdata = tv.datasets.CIFAR10(root='TestData',
+                                  train=False,download=False,transform=SimCLRDataTransform(augment))
+  
+  simclr = Validate()
+  testloader = simclr.load_data(testdata)
+  accuracy =  simclr.validate_labels(modelpath,testloader)
+  return accuracy
 
+def end_to_end():
+  s=1
+  input_shape = (96,96,3)  
+
+  # set up training data
+  train_trans = TrainDataAugmentation(s, input_shape)
+  train_aug = train_trans.augment()
+
+  train_data = tv.datasets.CIFAR10(
+    root='TrainData', train=True, download=True, 
+    transform=SimCLRDataTransform(train_aug)
+  )
+
+  # set up tuning data
+  tune_trans = TuneDataAugmentation(s, input_shape)
+  tune_aug = tune_trans.augment()
+
+  tune_data = tv.datasets.CIFAR10(
+    root='TuneData', train=True, download=True, 
+    transform=TuneDataTransform(tune_aug)
+  )
+
+  # set up val data
+  val_data = tv.datasets.CIFAR10(
+    root='TuneData', train=False, download=True, 
+    transform=TuneDataTransform(tune_aug)
+  )
+
+  # create the base model
+  model = ResNetSimCLR(in_channels=3, n_classes=10, mlp_layers=2)
+  simclr = SimCLR(model)
+
+  results = simclr.full_model_maker(
+    train_data, tune_data, val_data, n_cycles=2, train_epochs=1, tune_epochs=1,
+    train_path='chkpt/train.tar', tune_path='chkpt/tune.tar'
+  )
+
+  return results
 
 if __name__ == "__main__":
 #    model, losses = trainCIFAR()
-    i_accuracy, j_accuracy = validateCIFAR('CIFAR10.tar81')
+  # accuracy = validateCIFAR('CIFAR10.tar81')
+  model, train_loss, tune_loss, accuracy, actual, predicted = end_to_end()
+
+  plt.plot(train_loss)
+  plt.title('train loss')
+  plt.savefig('vis/train_losses.png')
+  plt.clf()
+
+  plt.plot(tune_loss)
+  plt.title('tune losses')
+  plt.savefig('vis/tune_losses.png')
+  plt.clf()
+
+  plt.plot(accuracy)
+  plt.title('accuracy')
+  plt.savefig('vis/accuracy.png')
+  plt.clf()
+
+
+  classes = [
+    'airplanes', 'cars', 'birds', 'cats', 'deer', 'dogs', 'frogs', 
+    'horses', 'ships', 'trucks'
+  ]
+
+  matrix = confusion_matrix(actual, predicted, [0,1,2,3,4,5,6,7,8,9])
+
+  figure = plt.figure(figsize=(16,9))
+  ax = figure.add_subplot(111)
+
+
+  disp = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=classes)
+  disp.plot(ax=ax)
+
+  plt.savefig('vis/confusion_matrix.png')
+  plt.clf()
+
